a440ced170a9e0c0c8819e4e637ae432c9cd5318
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-12-20 16:45:18 -0500

    Complete challenges

--- names/names.py
+++ names/names.py
@@ -1,24 +1,365 @@
 import time
 
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    """Wrap the given value in a ListNode and insert it
+    after this node. Note that this node could already
+    have a next node it is point to."""
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    """Wrap the given value in a ListNode and insert it
+    before this node. Note that this node could already
+    have a previous node it is point to."""
+
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+
+    """Rearranges this ListNode's previous and next pointers
+    accordingly, effectively deleting this ListNode."""
+
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
+
+"""Our doubly-linked list class. It holds references to
+the list's head and tail nodes."""
+
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new head of the list. Don't forget to handle 
+    the old head node's previous pointer accordingly."""
+
+    def add_to_head(self, value):
+        new_node = ListNode(value, None, self.head)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head.prev = new_node
+            self.head = new_node
+
+        self.length += 1
+
+    """Removes the List's current head node, making the
+    current head's next node the new head of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_head(self):
+        value = self.head.value
+        self.delete(self.head)
+        return value
+
+    """Wraps the given value in a ListNode and inserts it 
+    as the new tail of the list. Don't forget to handle 
+    the old tail node's next pointer accordingly."""
+
+    def add_to_tail(self, value):
+        new_node = ListNode(value, self.tail, None)
+
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+
+    """Removes the List's current tail node, making the 
+    current tail's previous node the new tail of the List.
+    Returns the value of the removed Node."""
+
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new head node of the List."""
+
+    def move_to_front(self, node):
+        if node is self.head:
+            return
+
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    """Removes the input node from its current spot in the 
+    List and inserts it as the new tail node of the List."""
+
+    def move_to_end(self, node):
+        if node is self.tail:
+            return
+
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+    """Removes a node from the list and handles cases where
+    the node was the head or the tail"""
+
+    def delete(self, node):
+        self.length -= 1
+
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+
+    """Returns the highest value currently in the list"""
+
+    def get_max(self):
+        if not self.head:
+            return None
+
+        max_val = self.head.value
+        current = self.head
+
+        while current:
+            if current.value > max_val:
+                max_val = current.value
+            current = current.next
+
+        return max_val
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def enqueue(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size is not 0:
+            value = self.storage.remove_from_head()
+            self.size -= 1
+
+            return value
+        else:
+            return None
+
+    def len(self):
+        return self.size
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        # Why is our DLL a good choice to store our elements?
+        self.storage = DoublyLinkedList()
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size is not 0:
+            value = self.storage.remove_from_tail()
+            self.size -= 1
+
+            return value
+        else:
+            return None
+
+    def len(self):
+        return self.size
+
+
+class BinarySearchTree:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # If inserting we must already have a tree/root
+        # if value is less than self.value go left, make a new tree/node if empty, otherwise
+        # keep going (recursion)
+        if self.value == None:
+            self.value = value
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+        # if greater than or equal to then go right, make a new tree/node if empty, otherwise
+        # keep going.
+        elif value >= self.value:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        # if target = self.value return it
+        # go left or right based on smaller or bigger
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.contains(target)
+        elif target > self.value:
+            if self.right == None:
+                return False
+            else:
+                return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        # start here, easy
+        # if right exists, go right
+        # otherwise return self.value
+        if self.right != None:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    # Call the function `cb` on the value of each node
+    # You may use a recursive or iterative approach
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
+    # DAY 2 Project -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+
+    def in_order_print(self, node):
+        if node == None:
+            return
+
+        if node.left != None:
+            self.in_order_print(node.left)
+
+        print(node.value)
+
+        if node.right != None:
+            self.in_order_print(node.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+
+    def bft_print(self, node):
+        node_queue = Queue()
+
+        node_queue.enqueue(node)
+
+        while node_queue.len() > 0:
+            current_node = node_queue.dequeue()
+            print(current_node.value)
+
+            if current_node.left != None:
+                node_queue.enqueue(current_node.left)
+            if current_node.right != None:
+                node_queue.enqueue(current_node.right)
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        node_stack = Stack()
+
+        node_stack.push(node)
+
+        while node_stack.len() > 0:
+            current_node = node_stack.pop()
+            print(current_node.value)
+
+            if current_node.left != None:
+                node_stack.push(current_node.left)
+            if current_node.right != None:
+                node_stack.push(current_node.right)
+
+
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+names_bst = BinarySearchTree()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")
+
+for name in names_1:
+    names_bst.insert(name)
+
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")
+
+for name in names_2:
+    if names_bst.contains(name):
+        duplicates.append(name)
+
+f.close()
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
